2b2c945abaf11a1c55d3dba302d279858ca794a0
Haripriya <mhpriya09@gmail.com>
2024-10-26 17:33:45 +0530

    Day5:Readme.md

---
+++ react/sample_project/src/App.jsx
@@ -0,0 +1,18 @@
+import Navbar from "./components/Navbar"
+const App = () => {
+ const data={
+    appname:"hello"
+ }
+ const logodata="mylogo"
+ const test="hbb"
+
+ return(
+    <>
+    <p className="text-blue-600">{test}</p>
+    <Navbar appdata={data} logo={logodata}/>
+    </>
+ )
+  
+}
+
+export default App
