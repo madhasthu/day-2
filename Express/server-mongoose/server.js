55261980c9050dee0239aaea9b0fdf16659c9b43
Haripriya <mhpriya09@gmail.com>
2024-11-07 09:23:05 +0530

    day-11
    Auth add (Bcrypt)

--- Express/server-mongoose/server.js
+++ Express/server-mongoose/server.js
@@ -1,5 +1,5 @@
 
-//import express, { json } from 'express'
+/*//import express, { json } from 'express'
 const express = require('express')
 const app = express()
 const port = 3000
@@ -12,4 +12,23 @@ app.get('/', (req, res) => res.status(200).json({ message: "Welcome" }))
 //localhost:3000/
 app.use('/products',Products)
 //localhost:3000/products
+app.listen(port, (() => console.log(`Listening on ${port}`)))*/
+
+
+
+const express = require('express')
+const app = express()
+const port = 3000
+app.use(express.json())
+const db = require('./config/db')
+const Products = require('./routes/ProductRoute')
+const Users = require('./routes/UserRoute')
+const Orders = require('./routes/OrderRoute')
+const Auth = require('./routes/AuthRoute')
+app.get('/', (req, res) => res.status(200).json({ message: "Welcome" }))
+app.use('/products', Products)
+app.use('/users', Users)
+app.use('/orders', Orders)
+app.use('/auth', Auth)
+
 app.listen(port, (() => console.log(`Listening on ${port}`)))
